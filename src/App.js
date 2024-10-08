0a31b3f57bea8304af13e99aea010c3b4ca68f1d
SaraCaste <sara.castellanos@fau.de>
2024-10-08 10:28:07 +0200

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,15 +1,31 @@
-// src/App.js
-import React from 'react';
+import React, { useState } from 'react';
 import FileUploader from './components/FileUploader';
+import 'bootstrap/dist/css/bootstrap.css';
 
 function App() {
+  const [setSelectedFile] = useState(null);
+
   return (
-    <div className="App">
-      <FileUploader />
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-1"></div>
+        <h1 class="col-11">Data Donation Platform</h1>
+      </div>
+
+      <div class="row mb-4">
+        <div class="col"> </div>
+      </div>
+
+      <div class="row mb-3">
+        {/* FileUploader handles file selection and upload */}
+        <div class="col-1"></div>
+        <div class="col-11">
+          <FileUploader onFileSelect={setSelectedFile} />
+        </div>
+        <div class="col-11"></div>
+        </div>
     </div>
   );
 }
 
 export default App;
-
-
