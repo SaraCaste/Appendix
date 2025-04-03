4b8e0c160c8600c6df3014dd8424513adc85763b
SaraCaste <sara.castellanos@fau.de>
2025-04-03 22:31:01 +0200

    Update app.py

--- Appendix A/data-enriching-platform/data-matching-module/backend/app.py
+++ Appendix A/data-enriching-platform/data-matching-module/backend/app.py
@@ -22,7 +22,7 @@ SUPABASE_URL = "https://pokscockcjadaffhipop.supabase.co"
 SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBva3Njb2NrY2phZGFmZmhpcG9wIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mjc3ODExMDksImV4cCI6MjA0MzM1NzEwOX0.L4Dx2JA0QiNeLC0_7m1jIe2SaZ1zHopG2cEbrh208A8"  # Replace with your actual key
 BUCKET_NAME = 'Database Processing Module'
 BASE_FILENAME = 'OFF_db_p'
-FILE_NUMBERS = range(1, 7)  # File numbers 1 to 6
+FILE_NUMBERS = range(1, 7)  
 
 # Initialize Supabase client
 supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
