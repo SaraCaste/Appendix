25b118e3318c784331674e6fd6dde48ab3fc6d60
SaraCaste <sara.castellanos@fau.de>
2025-04-04 10:03:23 +0200

    Update vite.config.js

--- Appendix A/data-donation-platform/vite.config.js
+++ Appendix A/data-donation-platform/vite.config.js
@@ -4,7 +4,18 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(() => {
   return {
     build: {
-      outDir: 'build',
+      rollupOptions: {
+        output: {
+          manualChunks(id) {
+            if (id.includes('node_modules')) {
+              return id
+                .split('node_modules')[1]
+                .split('/')[0]
+                .toString();
+            }
+          }
+        }
+      }
     },
     server: {
       open: true, 
